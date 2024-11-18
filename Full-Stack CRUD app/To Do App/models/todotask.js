19809ff96a50dfeb617f1aa7ba8410c35a21ed41
Ali Mousazada <mousazada.ali@gmail.com>
2024-11-17 19:35:43 -0500

    create ejs file and get and post method in server side

---
+++ Full-Stack CRUD app/To Do App/models/todotask.js
@@ -0,0 +1,34 @@
+// const mongoose = require("mongoose");
+// const todoTaskSchema = new mongoose.Schema({
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   content: {
+//     type: String,
+//     required: true,
+//   },
+//   date: {
+//     tyep: Date,
+//     default: Date.now,
+//   },
+// });
+
+// module.exports = mongoose.model("TodoTask", todoTaskSchema, "tasks");
+
+const mongoose = require('mongoose');
+const todoTaskSchema = new mongoose.Schema({
+title: {
+    type: String,
+    required: true
+},
+content: {
+    type: String,
+    required: true
+},
+date: {
+    type: Date,
+    default: Date.now
+}
+})
+module.exports = mongoose.model('TodoTask',todoTaskSchema,'tasks');
--- Full-Stack CRUD app/To Do App/server.js
+++ Full-Stack CRUD app/To Do App/server.js
@@ -2,40 +2,63 @@
 
 const express = require("express");
 const app = express();
-const PORT = 7800;
+const PORT = 7900;
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 // add model variable
+const TodoTask = require("./models/todotask");
+// const TodoTask = require("./models/TodoTask");
 
 //  set middleware
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// basic fundamental express method app.listen and app.get
 
-app.get("/", (req, res) => {
-  res.send("Hi, welcome to our app!");
+//  Connecting express to mongo DB
+
+mongoose
+.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("Connected to DB !");
+})
+.catch((err) => {
+  console.error("Database connection error:", err);
 });
 
-// mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
-//   console.log("Connected to DB !");
-// });
+// basic fundamental express method app.listen and app.get
+// GET METHOD
 
+app.get("/", async (request, response) => {
+  try {
+    const tasks = await TodoTask.find({});
+    response.render("index.ejs", {
+      todoTasks: tasks
+    });
+  } catch (error) {
+    response.status(500).send({ message: error.message });
+  }
+});
 
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to DB !");
-  })
-  .catch(err => {
-    console.error("Database connection error:", err);
+// POST
+app.post("/", async (req, res) => {
+  const todoTask = new TodoTask({
+    title: req.body.title,
+    content: req.body.content,
   });
-
-
-
-
-
+  try {
+    await todoTask.save();
+    console.log(todoTask);
+    res.redirect("/");
+  } catch (err) {
+    if (err) return res.status(500).send(err);
+    res.redirect("/");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
