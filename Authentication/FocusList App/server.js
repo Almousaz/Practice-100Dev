1ff156a06a04be033e1256a5e48152f2575ce37b
Ali Mousazada <mousazada.ali@gmail.com>
2024-11-26 10:49:26 -0500

    setup the structure / run server and config

---
+++ Authentication/FocusList App/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+require('dotenv').config({path: './config/.env'})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}, you better catch it!`)
+})    
