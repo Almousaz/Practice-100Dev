77ca8a7c990e8081da52f8e991da01c66121efe3
Ali Mousazada <mousazada.ali@gmail.com>
2024-12-16 20:53:21 -0500

    added auth server setup passport and session

--- Full-Stack CRUD app/Facebook Cat/server.js
+++ Full-Stack CRUD app/Facebook Cat/server.js
@@ -4,6 +4,12 @@ const app = express()
 const mongoose = require('mongoose')
 const connectDB = require('./config/connectDB')
 const catRoutes = require('./routes/catRoutes');
+const userRoutes = require('./routes/userRoutes');
+const passport = require('passport');
+const session = require('express-session');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/userModel'); // use the path to your User model
+
 
 const PORT = process.env.PORT || 5555
 
@@ -21,7 +27,29 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
+app.use(session({
+  secret: 'cats are awesome', 
+  resave: false,
+  saveUninitialized: false
+}));
 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// pass current user to all routes
+app.use((req, res, next) => {
+res.locals.currentUser = req.user;
+next();
+});
+
+
+app.use('/', userRoutes);
 app.use('/', catRoutes);
 
 
