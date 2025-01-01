de454f637df08742e9cbc0bd3bc3b8363de5e9fd
Ali Mousazada <mousazada.ali@gmail.com>
2025-01-01 00:21:39 -0500

    create new app to do

---
+++ MVC application/my app to do/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+const connectDB = require('./config/database')
+
+
+
+require('dotenv').config({path: './config/.env'})
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})   
